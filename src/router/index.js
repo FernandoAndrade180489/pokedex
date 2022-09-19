068f432f58d4480f9cbc607ca59a46b14718d8d0
Fernando Andrade <fernandow.and@gmail.com>
2022-09-19 16:41:07 -0300

    311. Exibindo os detalhes do Pokémon no palco

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import HabilidadesView from "../views/HabilidadesView.vue";
+import SobreView from "../views/SobreView.vue";
+import StatusView from "../views/StatusView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    children: [
+      {
+        path: "habilidades",
+        component: HabilidadesView,
+      },
+      {
+        path: "sobre",
+        component: SobreView,
+      },
+      {
+        path: "status",
+        component: StatusView,
+      },
+    ],
   },
 ];
 
